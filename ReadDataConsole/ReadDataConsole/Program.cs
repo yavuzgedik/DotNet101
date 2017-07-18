5172abc936cd05fb37bc703822886e51696583dc
yavuzgedik <yavuzgedik@windowslive.com>
2017-07-18 19:49:48 +0300

    Read Data Eklendi

---
+++ ReadDataConsole/ReadDataConsole/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadDataConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green; // Yazıların rengini yeşil yapar
+            Console.WriteLine("Lütfen Adınızı ve Soyadınızı Giriniz: ");
+            string gelenMetin = Console.ReadLine();
+
+            Console.WriteLine("Hoşgeldin " + gelenMetin + "\nçıkmak için bir tuşa basın.");
+            // aşağıdaki kod yerine \n 
+            //console.writeline("çıkmak için bir tuşa basın."); 
+
+            Console.Beep(); // Ses Çıkartır
+
+            Console.ReadKey();
+        }
+    }
+}
