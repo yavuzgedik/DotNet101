474630c2cef715adb6a74b90426640f9261d024c
yavuzgedik <yavuzgedik@windowslive.com>
2017-07-20 21:24:33 +0300

    FormsControl

---
+++ 07FormsControl/07FormsControl/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace _07FormsControl
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtMetin = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtMetin
+            // 
+            this.txtMetin.Location = new System.Drawing.Point(24, 15);
+            this.txtMetin.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.txtMetin.Name = "txtMetin";
+            this.txtMetin.Size = new System.Drawing.Size(482, 29);
+            this.txtMetin.TabIndex = 0;
+            this.txtMetin.TextChanged += new System.EventHandler(this.txtMetin_TextChanged);
+            this.txtMetin.DoubleClick += new System.EventHandler(this.txtMetin_DoubleClick);
+            this.txtMetin.MouseEnter += new System.EventHandler(this.txtMetin_MouseEnter);
+            this.txtMetin.MouseLeave += new System.EventHandler(this.txtMetin_MouseLeave);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 61);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 24);
+            this.label1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(521, 482);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtMetin);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtMetin;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ 07FormsControl/07FormsControl/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _07FormsControl
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txtMetin_TextChanged(object sender, EventArgs e)
+        {
+            label1.Text = txtMetin.Text;
+        }
+
+        private void txtMetin_MouseEnter(object sender, EventArgs e)
+        {
+            txtMetin.BackColor = Color.Maroon;
+            txtMetin.ForeColor = Color.Orange;
+        }
+
+        private void txtMetin_MouseLeave(object sender, EventArgs e)
+        {
+            txtMetin.BackColor = Color.White;
+            txtMetin.ForeColor = Color.Black;
+        }
+
+        private void txtMetin_DoubleClick(object sender, EventArgs e)
+        {
+            MessageBox.Show("Test");
+        }
+    }
+}
