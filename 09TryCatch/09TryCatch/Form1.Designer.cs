08e9f1a37d94299385c546e34b20b6acf29f85ad
yavuzgedik <yavuzgedik@windowslive.com>
2017-07-25 21:14:48 +0300

    TryCatch

---
+++ 09TryCatch/09TryCatch/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace _09TryCatch
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtDeger = new System.Windows.Forms.TextBox();
+            this.btnOnayla = new System.Windows.Forms.Button();
+            this.btnKontrolluOnay = new System.Windows.Forms.Button();
+            this.btnFinal = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtDeger
+            // 
+            this.txtDeger.Location = new System.Drawing.Point(39, 12);
+            this.txtDeger.Name = "txtDeger";
+            this.txtDeger.Size = new System.Drawing.Size(360, 29);
+            this.txtDeger.TabIndex = 0;
+            // 
+            // btnOnayla
+            // 
+            this.btnOnayla.Location = new System.Drawing.Point(39, 47);
+            this.btnOnayla.Name = "btnOnayla";
+            this.btnOnayla.Size = new System.Drawing.Size(360, 50);
+            this.btnOnayla.TabIndex = 1;
+            this.btnOnayla.Text = "Onayla";
+            this.btnOnayla.UseVisualStyleBackColor = true;
+            this.btnOnayla.Click += new System.EventHandler(this.btnOnayla_Click);
+            // 
+            // btnKontrolluOnay
+            // 
+            this.btnKontrolluOnay.Location = new System.Drawing.Point(39, 103);
+            this.btnKontrolluOnay.Name = "btnKontrolluOnay";
+            this.btnKontrolluOnay.Size = new System.Drawing.Size(360, 50);
+            this.btnKontrolluOnay.TabIndex = 2;
+            this.btnKontrolluOnay.Text = "Kontrollü Onay";
+            this.btnKontrolluOnay.UseVisualStyleBackColor = true;
+            this.btnKontrolluOnay.Click += new System.EventHandler(this.btnKontrolluOnay_Click);
+            // 
+            // btnFinal
+            // 
+            this.btnFinal.Location = new System.Drawing.Point(39, 159);
+            this.btnFinal.Name = "btnFinal";
+            this.btnFinal.Size = new System.Drawing.Size(360, 50);
+            this.btnFinal.TabIndex = 3;
+            this.btnFinal.Text = "Final Kontrollü Onay";
+            this.btnFinal.UseVisualStyleBackColor = true;
+            this.btnFinal.Click += new System.EventHandler(this.btnFinal_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(455, 365);
+            this.Controls.Add(this.btnFinal);
+            this.Controls.Add(this.btnKontrolluOnay);
+            this.Controls.Add(this.btnOnayla);
+            this.Controls.Add(this.txtDeger);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtDeger;
+        private System.Windows.Forms.Button btnOnayla;
+        private System.Windows.Forms.Button btnKontrolluOnay;
+        private System.Windows.Forms.Button btnFinal;
+    }
+}
+
---
+++ 09TryCatch/09TryCatch/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _09TryCatch
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOnayla_Click(object sender, EventArgs e)
+        {
+            long gelenDeger = long.Parse(txtDeger.Text);
+            //long gelenDeger = Convert.ToInt64(txtDeger.Text);
+
+            MessageBox.Show("Tebrikler, Iphone 8s plus kazandınız!");
+        }
+
+        private void btnKontrolluOnay_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Hata riski olan kodların yazıldığı alan
+
+                long gelenDeger = Convert.ToInt64(txtDeger.Text);
+                MessageBox.Show("Tebrikler, Iphone 8s plus kazandınız!");
+            }
+            catch (Exception hata)
+            {
+                // Hatalı işlem yapıldığında çalışan kod
+
+                //MessageBox.Show("Bir telefon numarası bile giremiyorsun!");
+
+                MessageBox.Show(hata.Message);
+            }
+        }
+
+        private void btnFinal_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                long gelenDeger = Convert.ToInt64(txtDeger.Text);
+                MessageBox.Show("Tebrikler, Iphone 8s plus kazandınız!");
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                MessageBox.Show("Hatasız Kul Olmaz, ben her türlü çalışırım");
+            }
+        }
+    }
+}
