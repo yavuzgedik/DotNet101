24bd4a21f563c337adc30c8699f0a3f44f7ecb1c
yavuzgedik <yavuzgedik@windowslive.com>
2017-07-25 21:40:16 +0300

    If_Else

---
+++ 10Karar_IF_ELSE/10Karar_IF_ELSE/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace _10Karar_IF_ELSE
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtDeger = new System.Windows.Forms.TextBox();
+            this.btnAdmin = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtDeger
+            // 
+            this.txtDeger.Location = new System.Drawing.Point(12, 12);
+            this.txtDeger.Name = "txtDeger";
+            this.txtDeger.Size = new System.Drawing.Size(228, 29);
+            this.txtDeger.TabIndex = 0;
+            this.txtDeger.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtDeger_KeyDown);
+            // 
+            // btnAdmin
+            // 
+            this.btnAdmin.Location = new System.Drawing.Point(12, 47);
+            this.btnAdmin.Name = "btnAdmin";
+            this.btnAdmin.Size = new System.Drawing.Size(228, 32);
+            this.btnAdmin.TabIndex = 1;
+            this.btnAdmin.Text = "Admin";
+            this.btnAdmin.UseVisualStyleBackColor = true;
+            this.btnAdmin.Click += new System.EventHandler(this.btnAdmin_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(252, 195);
+            this.Controls.Add(this.btnAdmin);
+            this.Controls.Add(this.txtDeger);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtDeger;
+        private System.Windows.Forms.Button btnAdmin;
+    }
+}
+
---
+++ 10Karar_IF_ELSE/10Karar_IF_ELSE/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _10Karar_IF_ELSE
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdmin_Click(object sender, EventArgs e)
+        {
+            if (txtDeger.Text.ToLower().Trim() == "admin") // iki değerin birbiri ile aynı olma durumu
+            {
+                MessageBox.Show("Hoşgeldin Admin");
+            }
+            else
+            {
+                MessageBox.Show("Tanıyamadım, Kimsiniz?");
+            }
+
+            txtDeger.Clear();
+            txtDeger.Focus();
+        }
+
+        private void txtDeger_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Enter)
+            {
+                btnAdmin_Click(sender, e);
+            }
+            else
+            {
+                this.Text = txtDeger.Text;
+            }
+        }
+    }
+}
