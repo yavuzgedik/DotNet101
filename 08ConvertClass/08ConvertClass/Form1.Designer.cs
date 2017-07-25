fb6db65d0068813989a9cd40f9bceb2697f61cdf
yavuzgedik <yavuzgedik@windowslive.com>
2017-07-25 20:08:08 +0300

    Convert İşlemleri

---
+++ 08ConvertClass/08ConvertClass/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace _08ConvertClass
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtSayi = new System.Windows.Forms.TextBox();
+            this.btn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtSayi
+            // 
+            this.txtSayi.Location = new System.Drawing.Point(12, 12);
+            this.txtSayi.Name = "txtSayi";
+            this.txtSayi.Size = new System.Drawing.Size(343, 29);
+            this.txtSayi.TabIndex = 0;
+            // 
+            // btn
+            // 
+            this.btn.Location = new System.Drawing.Point(361, 12);
+            this.btn.Name = "btn";
+            this.btn.Size = new System.Drawing.Size(87, 31);
+            this.btn.TabIndex = 1;
+            this.btn.Text = "Hesapla";
+            this.btn.UseVisualStyleBackColor = true;
+            this.btn.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(455, 338);
+            this.Controls.Add(this.btn);
+            this.Controls.Add(this.txtSayi);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtSayi;
+        private System.Windows.Forms.Button btn;
+    }
+}
+
---
+++ 08ConvertClass/08ConvertClass/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _08ConvertClass
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int sayi = 38;
+            string mesaj = sayi.ToString();
+            sayi++;
+            //MessageBox.Show(sayi.ToString());
+
+            MessageBox.Show("Sayi: " + sayi);
+            MessageBox.Show("Mesaj: " + mesaj);
+        }
+
+        private void btn_Click(object sender, EventArgs e)
+        {
+            string cevrilecekDeger = txtSayi.Text;
+
+            MessageBox.Show("Int : " + Convert.ToInt32(cevrilecekDeger));
+            MessageBox.Show("Byte : " + Convert.ToByte(cevrilecekDeger));
+            MessageBox.Show("Float : " + Convert.ToSingle(cevrilecekDeger));
+
+            bool burak = true;
+            MessageBox.Show(burak.ToString());
+        }
+    }
+}
