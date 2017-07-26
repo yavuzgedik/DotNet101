e5481ca8ae200f500c74616f0a3148f957b5f623
yavuzgedik <yavuzgedik@windowslive.com>
2017-07-26 20:49:58 +0300

    TernaryIF

---
+++ 11TernaryIF/TernaryIF/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TernaryIF
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int sayi = int.Parse(textBox1.Text);
+
+            int mutlakDeger = (sayi < 0) ? -1 * sayi : sayi;
+
+            MessageBox.Show(mutlakDeger + "");
+        }
+    }
+}
