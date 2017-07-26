c89f0df55773cf62f98b92d3191f66ffcc105cbd
yavuzgedik <yavuzgedik@windowslive.com>
2017-07-26 20:13:25 +0300

    IfElse_Güncelleme

--- 10Karar_IF_ELSE/10Karar_IF_ELSE/Form1.Designer.cs
+++ 10Karar_IF_ELSE/10Karar_IF_ELSE/Form1.Designer.cs
@@ -30,6 +30,10 @@
         {
             this.txtDeger = new System.Windows.Forms.TextBox();
             this.btnAdmin = new System.Windows.Forms.Button();
+            this.btnNot = new System.Windows.Forms.Button();
+            this.btnCiftTek = new System.Windows.Forms.Button();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // txtDeger
@@ -50,15 +54,59 @@
             this.btnAdmin.UseVisualStyleBackColor = true;
             this.btnAdmin.Click += new System.EventHandler(this.btnAdmin_Click);
             // 
+            // btnNot
+            // 
+            this.btnNot.Location = new System.Drawing.Point(12, 85);
+            this.btnNot.Name = "btnNot";
+            this.btnNot.Size = new System.Drawing.Size(228, 32);
+            this.btnNot.TabIndex = 2;
+            this.btnNot.Text = "Not";
+            this.btnNot.UseVisualStyleBackColor = true;
+            this.btnNot.Click += new System.EventHandler(this.btnNot_Click);
+            // 
+            // btnCiftTek
+            // 
+            this.btnCiftTek.Location = new System.Drawing.Point(12, 123);
+            this.btnCiftTek.Name = "btnCiftTek";
+            this.btnCiftTek.Size = new System.Drawing.Size(228, 32);
+            this.btnCiftTek.TabIndex = 3;
+            this.btnCiftTek.Text = "Çift Tek";
+            this.btnCiftTek.UseVisualStyleBackColor = true;
+            this.btnCiftTek.Click += new System.EventHandler(this.btnCiftTek_Click);
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(12, 173);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(228, 29);
+            this.txtPassword.TabIndex = 4;
+            this.txtPassword.Enter += new System.EventHandler(this.txtPassword_Enter);
+            this.txtPassword.Leave += new System.EventHandler(this.txtPassword_Leave);
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(12, 208);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(228, 32);
+            this.btnLogin.TabIndex = 5;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(252, 195);
+            this.ClientSize = new System.Drawing.Size(252, 262);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.btnCiftTek);
+            this.Controls.Add(this.btnNot);
             this.Controls.Add(this.btnAdmin);
             this.Controls.Add(this.txtDeger);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Margin = new System.Windows.Forms.Padding(6);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -70,6 +118,10 @@
 
         private System.Windows.Forms.TextBox txtDeger;
         private System.Windows.Forms.Button btnAdmin;
+        private System.Windows.Forms.Button btnNot;
+        private System.Windows.Forms.Button btnCiftTek;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Button btnLogin;
     }
 }
 
--- 10Karar_IF_ELSE/10Karar_IF_ELSE/Form1.cs
+++ 10Karar_IF_ELSE/10Karar_IF_ELSE/Form1.cs
@@ -43,5 +43,84 @@ namespace _10Karar_IF_ELSE
                 this.Text = txtDeger.Text;
             }
         }
+
+        private void btnNot_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int not = Convert.ToInt32(txtDeger.Text);
+
+                if (not < 0)
+                {
+                    MessageBox.Show("0 dan küçük değer giremezsiniz!");
+                }
+                else if (not > 100)
+                {
+                    MessageBox.Show("100 den büyük değer giremezsiniz!");
+                }
+                else
+                {
+                    MessageBox.Show("Hatasız not girişi yaptınız.");
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Not gireceksen sence de bir sayı yazman gerekmez miydi?");
+            }
+            
+        }
+
+        private void btnCiftTek_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int sayi = Convert.ToInt32(txtDeger.Text);
+
+                // Kolayca string'e çevirme yöntemi (string ile birleştirmek) 
+                //string strSayi = sayi + "";
+                //MessageBox.Show(strSayi);
+
+                if(sayi % 2 == 0)
+                {
+                    this.Text = sayi + " Çifttir";
+                }
+                else
+                {
+                    this.Text = sayi + " Tektir";
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Sayı Girseydin Keşke!");
+            }
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            txtPassword.PasswordChar = '?';
+
+            if(txtPassword.Text.Length >= 8)
+            {
+                MessageBox.Show("Giriş Onaylandı!");
+            }
+            else
+            {
+                MessageBox.Show("Daha Uzun Bir Şifre Girmeniz Gerekiyor!");
+            }
+
+            txtPassword.BackColor = Color.Green;
+        }
+
+        private void txtPassword_Enter(object sender, EventArgs e)
+        {
+            txtPassword.BackColor = Color.Red;
+            txtPassword.ForeColor = Color.White;
+        }
+
+        private void txtPassword_Leave(object sender, EventArgs e)
+        {
+            txtPassword.BackColor = Color.White;
+            txtPassword.ForeColor = Color.Black;
+        }
     }
 }
