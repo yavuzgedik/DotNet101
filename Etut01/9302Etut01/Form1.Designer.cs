ba26dc3d9eac0c9e807a664529cc453a2a329ab7
yavuzgedik <yavuzgedik@windowslive.com>
2017-07-31 19:37:42 +0300

    Güncellendi

---
+++ Etut01/9302Etut01/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace _9302Etut01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtAdSoyad = new System.Windows.Forms.TextBox();
+            this.lblMesaj = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtAdSoyad
+            // 
+            this.txtAdSoyad.ForeColor = System.Drawing.Color.Purple;
+            this.txtAdSoyad.Location = new System.Drawing.Point(12, 12);
+            this.txtAdSoyad.Name = "txtAdSoyad";
+            this.txtAdSoyad.Size = new System.Drawing.Size(100, 20);
+            this.txtAdSoyad.TabIndex = 0;
+            this.txtAdSoyad.TextChanged += new System.EventHandler(this.txtAdSoyad_TextChanged);
+            // 
+            // lblMesaj
+            // 
+            this.lblMesaj.AutoSize = true;
+            this.lblMesaj.BackColor = System.Drawing.Color.Transparent;
+            this.lblMesaj.ForeColor = System.Drawing.Color.Purple;
+            this.lblMesaj.Location = new System.Drawing.Point(9, 172);
+            this.lblMesaj.Name = "lblMesaj";
+            this.lblMesaj.Size = new System.Drawing.Size(16, 13);
+            this.lblMesaj.TabIndex = 1;
+            this.lblMesaj.Text = "...";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.BackgroundImage = global::_9302Etut01.Properties.Resources._6642624;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.lblMesaj);
+            this.Controls.Add(this.txtAdSoyad);
+            this.Name = "Form1";
+            this.Text = "9302 Etüt 01";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.DoubleClick += new System.EventHandler(this.Form1_DoubleClick);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtAdSoyad;
+        private System.Windows.Forms.Label lblMesaj;
+    }
+}
+
---
+++ Etut01/9302Etut01/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _9302Etut01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("Yükleniyorum...");
+        }
+
+        private void Form1_DoubleClick(object sender, EventArgs e)
+        {
+            MessageBox.Show("Çift Tikladi");
+        }
+
+        private void txtAdSoyad_TextChanged(object sender, EventArgs e)
+        {
+            Text = "Hoşgeldin " + txtAdSoyad.Text;
+            lblMesaj.Text = Text;
+        }
+    }
+}
