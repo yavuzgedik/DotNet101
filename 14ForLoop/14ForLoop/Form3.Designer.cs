b6307c17d3b699801681db030e9cac9ec2866188
yavuzgedik <yavuzgedik@windowslive.com>
2017-08-03 19:38:16 +0300

    İçi İçe For Döngüleri

--- 14ForLoop/14ForLoop/Form3.Designer.cs
+++ 14ForLoop/14ForLoop/Form3.Designer.cs
@@ -32,13 +32,19 @@
             this.label1 = new System.Windows.Forms.Label();
             this.btnAltAltaX = new System.Windows.Forms.Button();
             this.btnKare = new System.Windows.Forms.Button();
+            this.btnDikUcgen = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btnCarpimTablosu = new System.Windows.Forms.Button();
+            this.btnBreak = new System.Windows.Forms.Button();
+            this.btnContinue = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnYanYanaX
             // 
-            this.btnYanYanaX.Location = new System.Drawing.Point(12, 12);
+            this.btnYanYanaX.Location = new System.Drawing.Point(10, 11);
+            this.btnYanYanaX.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnYanYanaX.Name = "btnYanYanaX";
-            this.btnYanYanaX.Size = new System.Drawing.Size(195, 38);
+            this.btnYanYanaX.Size = new System.Drawing.Size(159, 32);
             this.btnYanYanaX.TabIndex = 0;
             this.btnYanYanaX.Text = "YAN YANA X";
             this.btnYanYanaX.UseVisualStyleBackColor = true;
@@ -48,16 +54,17 @@
             // 
             this.label1.BackColor = System.Drawing.Color.Black;
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(12, 199);
+            this.label1.Location = new System.Drawing.Point(10, 166);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(195, 180);
+            this.label1.Size = new System.Drawing.Size(159, 151);
             this.label1.TabIndex = 1;
             // 
             // btnAltAltaX
             // 
-            this.btnAltAltaX.Location = new System.Drawing.Point(12, 56);
+            this.btnAltAltaX.Location = new System.Drawing.Point(10, 46);
+            this.btnAltAltaX.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnAltAltaX.Name = "btnAltAltaX";
-            this.btnAltAltaX.Size = new System.Drawing.Size(195, 38);
+            this.btnAltAltaX.Size = new System.Drawing.Size(159, 32);
             this.btnAltAltaX.TabIndex = 2;
             this.btnAltAltaX.Text = "ALT ALTA X";
             this.btnAltAltaX.UseVisualStyleBackColor = true;
@@ -65,25 +72,84 @@
             // 
             // btnKare
             // 
-            this.btnKare.Location = new System.Drawing.Point(12, 100);
+            this.btnKare.Location = new System.Drawing.Point(10, 82);
+            this.btnKare.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnKare.Name = "btnKare";
-            this.btnKare.Size = new System.Drawing.Size(195, 38);
+            this.btnKare.Size = new System.Drawing.Size(159, 32);
             this.btnKare.TabIndex = 3;
             this.btnKare.Text = "KARE ÇİZ";
             this.btnKare.UseVisualStyleBackColor = true;
             this.btnKare.Click += new System.EventHandler(this.btnKare_Click);
             // 
+            // btnDikUcgen
+            // 
+            this.btnDikUcgen.Location = new System.Drawing.Point(10, 118);
+            this.btnDikUcgen.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnDikUcgen.Name = "btnDikUcgen";
+            this.btnDikUcgen.Size = new System.Drawing.Size(159, 32);
+            this.btnDikUcgen.TabIndex = 4;
+            this.btnDikUcgen.Text = "DİK ÜÇGEN ÇİZ";
+            this.btnDikUcgen.UseVisualStyleBackColor = true;
+            this.btnDikUcgen.Click += new System.EventHandler(this.btnDikUcgen_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(175, 13);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(161, 304);
+            this.listBox1.TabIndex = 5;
+            // 
+            // btnCarpimTablosu
+            // 
+            this.btnCarpimTablosu.Location = new System.Drawing.Point(342, 13);
+            this.btnCarpimTablosu.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnCarpimTablosu.Name = "btnCarpimTablosu";
+            this.btnCarpimTablosu.Size = new System.Drawing.Size(159, 32);
+            this.btnCarpimTablosu.TabIndex = 6;
+            this.btnCarpimTablosu.Text = "ÇARPIM TABLOSU";
+            this.btnCarpimTablosu.UseVisualStyleBackColor = true;
+            this.btnCarpimTablosu.Click += new System.EventHandler(this.btnCarpimTablosu_Click);
+            // 
+            // btnBreak
+            // 
+            this.btnBreak.Location = new System.Drawing.Point(342, 49);
+            this.btnBreak.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnBreak.Name = "btnBreak";
+            this.btnBreak.Size = new System.Drawing.Size(159, 32);
+            this.btnBreak.TabIndex = 7;
+            this.btnBreak.Text = "BREAK";
+            this.btnBreak.UseVisualStyleBackColor = true;
+            this.btnBreak.Click += new System.EventHandler(this.btnBreak_Click);
+            // 
+            // btnContinue
+            // 
+            this.btnContinue.Location = new System.Drawing.Point(342, 85);
+            this.btnContinue.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnContinue.Name = "btnContinue";
+            this.btnContinue.Size = new System.Drawing.Size(159, 32);
+            this.btnContinue.TabIndex = 8;
+            this.btnContinue.Text = "CONTINUE";
+            this.btnContinue.UseVisualStyleBackColor = true;
+            this.btnContinue.Click += new System.EventHandler(this.btnContinue_Click);
+            // 
             // Form3
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(670, 388);
+            this.ClientSize = new System.Drawing.Size(509, 322);
+            this.Controls.Add(this.btnContinue);
+            this.Controls.Add(this.btnBreak);
+            this.Controls.Add(this.btnCarpimTablosu);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnDikUcgen);
             this.Controls.Add(this.btnKare);
             this.Controls.Add(this.btnAltAltaX);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.btnYanYanaX);
-            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form3";
             this.Text = "Form3";
             this.ResumeLayout(false);
@@ -96,5 +162,10 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button btnAltAltaX;
         private System.Windows.Forms.Button btnKare;
+        private System.Windows.Forms.Button btnDikUcgen;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btnCarpimTablosu;
+        private System.Windows.Forms.Button btnBreak;
+        private System.Windows.Forms.Button btnContinue;
     }
 }
--- 14ForLoop/14ForLoop/Form3.cs
+++ 14ForLoop/14ForLoop/Form3.cs
@@ -39,24 +39,96 @@ namespace _14ForLoop
 
         private void btnKare_Click(object sender, EventArgs e)
         {
-            label1.Text = "";
+
 
             for (int i = 0; i < 5; i++)
             {
                 for (int j = 0; j < 5; j++)
                 {
-                    if ((j > 0 && j < 4) && (i > 0 && i < 4))
-                    {
-                        label1.Text += "     ";
-                    }
-                    else
-                    {
-                        label1.Text += "X  ";
-                    }
+                    // İçi boş kare
+                    //if ((j > 0 && j < 4) && (i > 0 && i < 4))
+                    //{
+                    //    label1.Text += "    ";
+                    //}
+                    //else
+                    //{
+                    //    label1.Text += "X  ";
+                    //}
 
-                    //label1.Text += "X  ";
+                    // içi dolu kare
+                    label1.Text += "X  ";
                 }
                 label1.Text += "\n";
+            }
+        }
+
+        private void btnDikUcgen_Click(object sender, EventArgs e)
+        {
+            label1.Text = "";
+
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    label1.Text += "X  ";
+                }
+                label1.Text += "\n";
+            }
+        }
+
+        private void btnCarpimTablosu_Click(object sender, EventArgs e)
+        {
+            // 3 X 5 = 15
+
+            for (int i = 1; i <= 10; i++)
+            {
+                for (int j = 1; j <= 10; j++)
+                {
+                    //listBox1.Items.Add(i + " X " + j + " = " + i * j);
+
+                    //listBox1.Items.Add($"{i} X {j} = {i * j}");
+
+                    listBox1.Items.Add(string.Format("{0} X {1} = {2}", i, j, i * j));
+                }
+                listBox1.Items.Add("-----------");
+            }
+        }
+
+        private void btnBreak_Click(object sender, EventArgs e)
+        {
+            int i = 0;
+            for (i = 0; i < 100; i++)
+            {
+                if (i == 20)
+                {
+                    listBox1.Items.Add(i);
+                    break;
+                }
+                listBox1.Items.Add(i);
+            }
+
+            listBox1.Items.Add(i);
+        }
+
+        private void btnContinue_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+
+            Random rnd = new Random();
+
+            for (int i = 0; i < 8; i++)
+            {
+                int sayi = rnd.Next(1, 10);
+
+                if (listBox1.Items.Contains(sayi))
+                {
+                    continue; // Continue bir sonraki iterasyona gönderir
+                }
+                else
+                {
+                    listBox1.Items.Add(sayi);
+                }
+                listBox1.Items.Add(11);
             }
         }
     }
