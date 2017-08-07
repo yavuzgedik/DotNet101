abdf0fb326be81205680524fc5fa72b159222ab9
yavuzgedik <yavuzgedik@windowslive.com>
2017-08-07 21:24:46 +0300

    Foreach Eklendi

---
+++ 17ForEachLoop/17ForEachLoop/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace _17ForEachLoop
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btnIki = new System.Windows.Forms.Button();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Location = new System.Drawing.Point(13, 13);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(222, 43);
+            this.btnEkle.TabIndex = 0;
+            this.btnEkle.Text = "Elemanları ListBox\'a Ekle";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(242, 13);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 184);
+            this.listBox1.TabIndex = 1;
+            // 
+            // btnIki
+            // 
+            this.btnIki.Location = new System.Drawing.Point(14, 62);
+            this.btnIki.Name = "btnIki";
+            this.btnIki.Size = new System.Drawing.Size(222, 43);
+            this.btnIki.TabIndex = 2;
+            this.btnIki.Text = "İkiye Bölünenler";
+            this.btnIki.UseVisualStyleBackColor = true;
+            this.btnIki.Click += new System.EventHandler(this.btnIki_Click);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 20;
+            this.listBox2.Location = new System.Drawing.Point(368, 13);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(120, 184);
+            this.listBox2.TabIndex = 3;
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.ItemHeight = 20;
+            this.listBox3.Location = new System.Drawing.Point(494, 13);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(120, 184);
+            this.listBox3.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(625, 221);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.btnIki);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnEkle);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btnIki;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ListBox listBox3;
+    }
+}
+
---
+++ 17ForEachLoop/17ForEachLoop/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _17ForEachLoop
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string[] sehirler =
+        {
+            "Ankara",
+            "İzmir",
+            "İstanbul",
+            "Manisa",
+            "Muş",
+            "Iğdır",
+            "Rize",
+            "Çorum"
+        };
+
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            foreach (string str in sehirler)
+            {
+                listBox1.Items.Add(str);
+            }
+
+            //for (int i = 0; i < sehirler.Length; i++)
+            //{
+            //    listBox1.Items.Add(sehirler[i]);
+            //}
+        }
+
+        private void btnIki_Click(object sender, EventArgs e)
+        {
+            int[] sayilar = { 123, 45, 56, 876, 753, 734, 11, 9, 2017 };
+
+            // hem 2 hemde 3 e tam bölünebilen kaç sayı var mesaj box da yazın
+            // 2 ye tam bölünen sayıları listbox1
+            // 3 e tam bölünen sayıları listbox2 ye ekleyin
+
+            int tamBolunenler = 0;
+            foreach (int sayi in sayilar)
+            {
+                // && Ve işareti solundaki ve sağındaki iki durumda sağlandığında
+                if (sayi % 2 == 0 && sayi % 3 == 0)
+                {
+                    tamBolunenler++;
+                }
+                
+
+                #region 2ye3ebölünenANDsiz
+                //if (sayi % 2 == 0)
+                //{
+                //    if (sayi % 3 == 0)
+                //    {
+                //        tamBolunenler++;
+                //    }
+                //}
+                #endregion
+
+                if (sayi % 2 == 0)
+                {
+                    listBox1.Items.Add(sayi);
+                }
+
+                if (sayi % 3 == 0)
+                {
+                    listBox2.Items.Add(sayi);
+                }
+                
+                if (sayi % 2 != 0 && sayi % 3 != 0)
+                {
+                    listBox3.Items.Add(sayi);
+                }
+            }
+
+            MessageBox.Show($"2 ve 3 e tam bölünen {tamBolunenler} sayi vardır.");
+        }
+    }
+}
