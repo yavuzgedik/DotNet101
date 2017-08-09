b6cb43a2029fb8e99c6714940906f706e3994b78
yavuzgedik <yavuzgedik@windowslive.com>
2017-08-09 21:30:38 +0300

    Quiz1

---
+++ Quiz1/Quiz1/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace Quiz1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtAd = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnKaydet = new System.Windows.Forms.Button();
+            this.lboxRehber = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSoyad = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtTel = new System.Windows.Forms.TextBox();
+            this.btnAra = new System.Windows.Forms.Button();
+            this.btnTümü = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtAd
+            // 
+            this.txtAd.Location = new System.Drawing.Point(81, 9);
+            this.txtAd.Name = "txtAd";
+            this.txtAd.Size = new System.Drawing.Size(137, 26);
+            this.txtAd.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Ad";
+            // 
+            // btnKaydet
+            // 
+            this.btnKaydet.BackColor = System.Drawing.Color.DarkGreen;
+            this.btnKaydet.ForeColor = System.Drawing.SystemColors.Control;
+            this.btnKaydet.Location = new System.Drawing.Point(119, 105);
+            this.btnKaydet.Name = "btnKaydet";
+            this.btnKaydet.Size = new System.Drawing.Size(99, 35);
+            this.btnKaydet.TabIndex = 3;
+            this.btnKaydet.Text = "Kaydet";
+            this.btnKaydet.UseVisualStyleBackColor = false;
+            this.btnKaydet.Click += new System.EventHandler(this.btnKaydet_Click);
+            // 
+            // lboxRehber
+            // 
+            this.lboxRehber.FormattingEnabled = true;
+            this.lboxRehber.ItemHeight = 20;
+            this.lboxRehber.Location = new System.Drawing.Point(224, 9);
+            this.lboxRehber.Name = "lboxRehber";
+            this.lboxRehber.Size = new System.Drawing.Size(358, 544);
+            this.lboxRehber.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 20);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "SoyAd";
+            // 
+            // txtSoyad
+            // 
+            this.txtSoyad.Location = new System.Drawing.Point(81, 41);
+            this.txtSoyad.Name = "txtSoyad";
+            this.txtSoyad.Size = new System.Drawing.Size(137, 26);
+            this.txtSoyad.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 79);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 20);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Telefon";
+            // 
+            // txtTel
+            // 
+            this.txtTel.Location = new System.Drawing.Point(81, 73);
+            this.txtTel.Name = "txtTel";
+            this.txtTel.Size = new System.Drawing.Size(137, 26);
+            this.txtTel.TabIndex = 2;
+            // 
+            // btnAra
+            // 
+            this.btnAra.BackColor = System.Drawing.Color.OrangeRed;
+            this.btnAra.ForeColor = System.Drawing.SystemColors.Control;
+            this.btnAra.Location = new System.Drawing.Point(12, 105);
+            this.btnAra.Name = "btnAra";
+            this.btnAra.Size = new System.Drawing.Size(99, 35);
+            this.btnAra.TabIndex = 4;
+            this.btnAra.Text = "Ara";
+            this.btnAra.UseVisualStyleBackColor = false;
+            this.btnAra.Click += new System.EventHandler(this.btnAra_Click);
+            // 
+            // btnTümü
+            // 
+            this.btnTümü.BackColor = System.Drawing.Color.Black;
+            this.btnTümü.ForeColor = System.Drawing.SystemColors.Control;
+            this.btnTümü.Location = new System.Drawing.Point(12, 146);
+            this.btnTümü.Name = "btnTümü";
+            this.btnTümü.Size = new System.Drawing.Size(206, 35);
+            this.btnTümü.TabIndex = 9;
+            this.btnTümü.Text = "Tümünü Getir";
+            this.btnTümü.UseVisualStyleBackColor = false;
+            this.btnTümü.Click += new System.EventHandler(this.btnTümü_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(594, 556);
+            this.Controls.Add(this.btnTümü);
+            this.Controls.Add(this.btnAra);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtTel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtSoyad);
+            this.Controls.Add(this.lboxRehber);
+            this.Controls.Add(this.btnKaydet);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtAd);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtAd;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnKaydet;
+        private System.Windows.Forms.ListBox lboxRehber;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtSoyad;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtTel;
+        private System.Windows.Forms.Button btnAra;
+        private System.Windows.Forms.Button btnTümü;
+    }
+}
+
---
+++ Quiz1/Quiz1/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Quiz1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string[] isimler = new string[100];
+        string[] soyIsimler = new string[100];
+        string[] telefonlar = new string[100];
+
+        int index = 0; 
+        private void btnKaydet_Click(object sender, EventArgs e)
+        {
+            string isim = txtAd.Text.Trim();
+            string soyIsim = txtSoyad.Text.Trim();
+            string tel = txtTel.Text.Trim();
+
+            isimler[index] = isim;
+            soyIsimler[index] = soyIsim;
+            telefonlar[index] = tel;
+
+            lboxRehber.Items.Add($"{isim} {soyIsim} \t {tel}");
+
+            index++;
+
+            txtAd.Clear();
+            txtSoyad.Clear();
+            txtTel.Clear();
+        }
+
+        private void btnAra_Click(object sender, EventArgs e)
+        {
+            lboxRehber.Items.Clear();
+
+            if (txtAd.Text != "")
+            {
+                for (int i = 0; i < index; i++)
+                {
+                    if (txtAd.Text.Trim().ToLower() == isimler[i].ToLower())
+                    {
+                        lboxRehber.Items.Add($"{isimler[i]} {soyIsimler[i]} \t {telefonlar[i]}");
+                    }
+                }
+            }
+            else if (txtSoyad.Text != "")
+            {
+                for (int i = 0; i < index; i++)
+                {
+                    if (txtSoyad.Text.Trim().ToLower() == soyIsimler[i].ToLower())
+                    {
+                        lboxRehber.Items.Add($"{isimler[i]} {soyIsimler[i]} \t {telefonlar[i]}");
+                    }
+                }
+            }
+            else if (txtTel.Text != "")
+            {
+                for (int i = 0; i < index; i++)
+                {
+                    if (txtTel.Text.Trim() == telefonlar[i])
+                    {
+                        lboxRehber.Items.Add($"{isimler[i]} {soyIsimler[i]} \t {telefonlar[i]}");
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Lütfen Ad, Soyad veya Telefon Numarası Girin!");
+            }
+
+            txtAd.Clear();
+            txtSoyad.Clear();
+            txtTel.Clear();
+        }
+
+        private void btnTümü_Click(object sender, EventArgs e)
+        {
+            lboxRehber.Items.Clear();
+
+            for (int i = 0; i < index; i++)
+            {
+                lboxRehber.Items.Add($"{isimler[i]} {soyIsimler[i]} \t {telefonlar[i]}");
+            }
+
+            txtAd.Clear();
+            txtSoyad.Clear();
+            txtTel.Clear();
+        }
+    }
+}
