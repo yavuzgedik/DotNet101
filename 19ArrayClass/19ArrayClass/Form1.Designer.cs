4f10795ed776085660399dff8ef92350c1abfbb5
yavuzgedik <yavuzgedik@windowslive.com>
2017-08-14 20:57:50 +0300

    ArrayClass Update

--- 19ArrayClass/19ArrayClass/Form1.Designer.cs
+++ 19ArrayClass/19ArrayClass/Form1.Designer.cs
@@ -30,14 +30,20 @@
         {
             this.btnClear = new System.Windows.Forms.Button();
             this.btnCopy = new System.Windows.Forms.Button();
+            this.btnLastIndexOf = new System.Windows.Forms.Button();
+            this.btnIndexOf = new System.Windows.Forms.Button();
+            this.txtMetin = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btnSort = new System.Windows.Forms.Button();
+            this.btnReverse = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnClear
             // 
-            this.btnClear.Location = new System.Drawing.Point(40, 20);
+            this.btnClear.Location = new System.Drawing.Point(13, 44);
             this.btnClear.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnClear.Name = "btnClear";
-            this.btnClear.Size = new System.Drawing.Size(352, 57);
+            this.btnClear.Size = new System.Drawing.Size(214, 32);
             this.btnClear.TabIndex = 0;
             this.btnClear.Text = "Array.Clear()";
             this.btnClear.UseVisualStyleBackColor = true;
@@ -45,20 +51,86 @@
             // 
             // btnCopy
             // 
-            this.btnCopy.Location = new System.Drawing.Point(40, 87);
+            this.btnCopy.Location = new System.Drawing.Point(13, 86);
             this.btnCopy.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnCopy.Name = "btnCopy";
-            this.btnCopy.Size = new System.Drawing.Size(352, 57);
+            this.btnCopy.Size = new System.Drawing.Size(214, 32);
             this.btnCopy.TabIndex = 1;
             this.btnCopy.Text = "Array.Copy()";
             this.btnCopy.UseVisualStyleBackColor = true;
             this.btnCopy.Click += new System.EventHandler(this.btnCopy_Click);
             // 
+            // btnLastIndexOf
+            // 
+            this.btnLastIndexOf.Location = new System.Drawing.Point(13, 128);
+            this.btnLastIndexOf.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnLastIndexOf.Name = "btnLastIndexOf";
+            this.btnLastIndexOf.Size = new System.Drawing.Size(214, 32);
+            this.btnLastIndexOf.TabIndex = 2;
+            this.btnLastIndexOf.Text = "Array.LastIndexOf()";
+            this.btnLastIndexOf.UseVisualStyleBackColor = true;
+            this.btnLastIndexOf.Click += new System.EventHandler(this.btnLastIndexOf_Click);
+            // 
+            // btnIndexOf
+            // 
+            this.btnIndexOf.Location = new System.Drawing.Point(13, 170);
+            this.btnIndexOf.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnIndexOf.Name = "btnIndexOf";
+            this.btnIndexOf.Size = new System.Drawing.Size(214, 32);
+            this.btnIndexOf.TabIndex = 3;
+            this.btnIndexOf.Text = "Array.IndexOf()";
+            this.btnIndexOf.UseVisualStyleBackColor = true;
+            this.btnIndexOf.Click += new System.EventHandler(this.btnIndexOf_Click);
+            // 
+            // txtMetin
+            // 
+            this.txtMetin.Location = new System.Drawing.Point(13, 13);
+            this.txtMetin.Name = "txtMetin";
+            this.txtMetin.Size = new System.Drawing.Size(214, 26);
+            this.txtMetin.TabIndex = 4;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(234, 22);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(180, 264);
+            this.listBox1.TabIndex = 5;
+            // 
+            // btnSort
+            // 
+            this.btnSort.Location = new System.Drawing.Point(13, 212);
+            this.btnSort.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnSort.Name = "btnSort";
+            this.btnSort.Size = new System.Drawing.Size(214, 32);
+            this.btnSort.TabIndex = 6;
+            this.btnSort.Text = "Array.Sort()";
+            this.btnSort.UseVisualStyleBackColor = true;
+            this.btnSort.Click += new System.EventHandler(this.btnSort_Click);
+            // 
+            // btnReverse
+            // 
+            this.btnReverse.Location = new System.Drawing.Point(13, 254);
+            this.btnReverse.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnReverse.Name = "btnReverse";
+            this.btnReverse.Size = new System.Drawing.Size(214, 32);
+            this.btnReverse.TabIndex = 7;
+            this.btnReverse.Text = "Array.Reverse()";
+            this.btnReverse.UseVisualStyleBackColor = true;
+            this.btnReverse.Click += new System.EventHandler(this.btnReverse_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(426, 506);
+            this.ClientSize = new System.Drawing.Size(426, 297);
+            this.Controls.Add(this.btnReverse);
+            this.Controls.Add(this.btnSort);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.txtMetin);
+            this.Controls.Add(this.btnIndexOf);
+            this.Controls.Add(this.btnLastIndexOf);
             this.Controls.Add(this.btnCopy);
             this.Controls.Add(this.btnClear);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
@@ -66,6 +138,7 @@
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -73,6 +146,12 @@
 
         private System.Windows.Forms.Button btnClear;
         private System.Windows.Forms.Button btnCopy;
+        private System.Windows.Forms.Button btnLastIndexOf;
+        private System.Windows.Forms.Button btnIndexOf;
+        private System.Windows.Forms.TextBox txtMetin;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btnSort;
+        private System.Windows.Forms.Button btnReverse;
     }
 }
 
--- 19ArrayClass/19ArrayClass/Form1.cs
+++ 19ArrayClass/19ArrayClass/Form1.cs
@@ -19,6 +19,7 @@ namespace _19ArrayClass
 
         string[] sehirler =
         {
+            "Adana",
             "İstanbul",
             "İzmir",
             "Ankara",
@@ -39,6 +40,64 @@ namespace _19ArrayClass
             Array.Copy(sehirler, ornek, 3);
 
             //Array.Copy(sehirler, 3, ornek, 0, 2);
+        }
+
+        private void btnLastIndexOf_Click(object sender, EventArgs e)
+        {
+            int index = Array.LastIndexOf(sehirler, "Adana");
+        }
+
+        private void btnIndexOf_Click(object sender, EventArgs e)
+        {
+            //int index = Array.IndexOf(sehirler, "İzmir");
+
+            string sehir = txtMetin.Text;
+
+            if (Array.IndexOf(sehirler, sehir) != -1)
+            {
+                if (Array.IndexOf(sehirler, sehir) == Array.LastIndexOf(sehirler, sehir))
+                {
+                    MessageBox.Show("Bu Şehirden Bir Tane Var");
+                }
+                else
+                {
+                    MessageBox.Show("Bu Şehirden Birden fazla Var");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Şehir Bulunamadı!");
+            }
+            
+        }
+
+        private void btnSort_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+
+            Array.Sort(sehirler);
+            foreach (var item in sehirler)
+            {
+                listBox1.Items.Add(item);
+            }
+
+            //int[] sayilar = { 45, 99, 75, 98, -1, 32 };
+            //Array.Sort(sayilar);
+            //foreach (var item in sayilar)
+            //{
+            //    listBox1.Items.Add(item);
+            //}
+        }
+
+        private void btnReverse_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+
+            Array.Reverse(sehirler);
+            foreach (var item in sehirler)
+            {
+                listBox1.Items.Add(item);
+            }
         }
     }
 }
