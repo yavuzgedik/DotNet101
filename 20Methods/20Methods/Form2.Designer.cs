f55b3f223fe8dc4a00a7aa264a447da778dfce46
yavuzgedik <yavuzgedik@windowslive.com>
2017-08-15 21:36:02 +0300

    Methods Güncellendi :Smile:

--- 20Methods/20Methods/Form2.Designer.cs
+++ 20Methods/20Methods/Form2.Designer.cs
@@ -31,6 +31,12 @@
             this.numBirinci = new System.Windows.Forms.NumericUpDown();
             this.btnTopla = new System.Windows.Forms.Button();
             this.numIkinci = new System.Windows.Forms.NumericUpDown();
+            this.txtSayi = new System.Windows.Forms.TextBox();
+            this.btnCiftTek = new System.Windows.Forms.Button();
+            this.btnRandom = new System.Windows.Forms.Button();
+            this.cboxColor = new System.Windows.Forms.ComboBox();
+            this.btnRandomColor = new System.Windows.Forms.Button();
+            this.btnWC = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.numBirinci)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numIkinci)).BeginInit();
             this.SuspendLayout();
@@ -59,11 +65,84 @@
             this.numIkinci.Size = new System.Drawing.Size(120, 20);
             this.numIkinci.TabIndex = 1;
             // 
+            // txtSayi
+            // 
+            this.txtSayi.Location = new System.Drawing.Point(12, 74);
+            this.txtSayi.Name = "txtSayi";
+            this.txtSayi.Size = new System.Drawing.Size(104, 20);
+            this.txtSayi.TabIndex = 3;
+            // 
+            // btnCiftTek
+            // 
+            this.btnCiftTek.Location = new System.Drawing.Point(122, 74);
+            this.btnCiftTek.Name = "btnCiftTek";
+            this.btnCiftTek.Size = new System.Drawing.Size(139, 20);
+            this.btnCiftTek.TabIndex = 4;
+            this.btnCiftTek.Text = "Ç T";
+            this.btnCiftTek.UseVisualStyleBackColor = true;
+            this.btnCiftTek.Click += new System.EventHandler(this.btnCiftTek_Click);
+            // 
+            // btnRandom
+            // 
+            this.btnRandom.Location = new System.Drawing.Point(12, 100);
+            this.btnRandom.Name = "btnRandom";
+            this.btnRandom.Size = new System.Drawing.Size(249, 44);
+            this.btnRandom.TabIndex = 5;
+            this.btnRandom.Text = "Random Game";
+            this.btnRandom.UseVisualStyleBackColor = true;
+            this.btnRandom.Click += new System.EventHandler(this.btnRandom_Click);
+            // 
+            // cboxColor
+            // 
+            this.cboxColor.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboxColor.FormattingEnabled = true;
+            this.cboxColor.Items.AddRange(new object[] {
+            "Blue",
+            "Black",
+            "Yellow",
+            "Red",
+            "Green",
+            "Gray",
+            "White",
+            "Purple",
+            "Orange"});
+            this.cboxColor.Location = new System.Drawing.Point(12, 150);
+            this.cboxColor.Name = "cboxColor";
+            this.cboxColor.Size = new System.Drawing.Size(249, 21);
+            this.cboxColor.TabIndex = 6;
+            this.cboxColor.SelectedIndexChanged += new System.EventHandler(this.cboxColor_SelectedIndexChanged);
+            // 
+            // btnRandomColor
+            // 
+            this.btnRandomColor.Location = new System.Drawing.Point(12, 177);
+            this.btnRandomColor.Name = "btnRandomColor";
+            this.btnRandomColor.Size = new System.Drawing.Size(249, 29);
+            this.btnRandomColor.TabIndex = 7;
+            this.btnRandomColor.Text = "Random Color";
+            this.btnRandomColor.UseVisualStyleBackColor = true;
+            this.btnRandomColor.Click += new System.EventHandler(this.btnRandomColor_Click);
+            // 
+            // btnWC
+            // 
+            this.btnWC.Location = new System.Drawing.Point(12, 212);
+            this.btnWC.Name = "btnWC";
+            this.btnWC.Size = new System.Drawing.Size(249, 29);
+            this.btnWC.TabIndex = 8;
+            this.btnWC.Text = "What Color?";
+            this.btnWC.UseVisualStyleBackColor = true;
+            this.btnWC.Click += new System.EventHandler(this.btnWC_Click);
+            // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(273, 259);
+            this.Controls.Add(this.btnWC);
+            this.Controls.Add(this.btnRandomColor);
+            this.Controls.Add(this.cboxColor);
+            this.Controls.Add(this.btnRandom);
+            this.Controls.Add(this.btnCiftTek);
+            this.Controls.Add(this.txtSayi);
             this.Controls.Add(this.numIkinci);
             this.Controls.Add(this.btnTopla);
             this.Controls.Add(this.numBirinci);
@@ -72,6 +151,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.numBirinci)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.numIkinci)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -80,5 +160,11 @@
         private System.Windows.Forms.NumericUpDown numBirinci;
         private System.Windows.Forms.Button btnTopla;
         private System.Windows.Forms.NumericUpDown numIkinci;
+        private System.Windows.Forms.TextBox txtSayi;
+        private System.Windows.Forms.Button btnCiftTek;
+        private System.Windows.Forms.Button btnRandom;
+        private System.Windows.Forms.ComboBox cboxColor;
+        private System.Windows.Forms.Button btnRandomColor;
+        private System.Windows.Forms.Button btnWC;
     }
 }
--- 20Methods/20Methods/Form2.cs
+++ 20Methods/20Methods/Form2.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,11 +18,42 @@ namespace _20Methods
             InitializeComponent();
         }
 
+        Random rnd = new Random();
+
         int Topla(int sayi1, int sayi2)
         {
             int toplam = sayi1 + sayi2;
             return toplam;
         }
+        string TekCift(int sayi)
+        {
+            if (sayi % 2 == 0)
+            {
+                return "Çift";
+            }
+            else
+            {
+                return "Tek";
+            }
+        }
+        void RenkDegistir(string color)
+        {
+            BackColor = Color.FromName(color);
+            Text = BackColor.Name;
+        }
+        void RenkDegistir()
+        {
+            int red = rnd.Next(0, 256);
+            int green = rnd.Next(0, 256);
+            int blue = rnd.Next(0, 256);
+
+            BackColor = Color.FromArgb(red, green, blue);
+        }
+        Color ArkaPlanRengi()
+        {
+            return BackColor;
+        }
+
         private void btnTopla_Click(object sender, EventArgs e)
         {
             int num1 = (int)numBirinci.Value;
@@ -29,6 +61,38 @@ namespace _20Methods
             int toplam = Topla(num1, num2);
 
             MessageBox.Show($"Girdiğin Sayıların Toplamı: {toplam}");
+        }
+        private void btnCiftTek_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(TekCift(int.Parse(txtSayi.Text)));
+        }
+        private void btnRandom_Click(object sender, EventArgs e)
+        {
+            int sayi = rnd.Next(1, 10000);
+
+            if(TekCift(sayi) == "Tek")
+            {
+                MessageBox.Show($"{sayi} tek olduğundan kaybettiniz!");
+            }
+            else
+            {
+                MessageBox.Show($"{sayi} çift olduğundan kazandınız!");
+            }
+        }
+        private void cboxColor_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            RenkDegistir(cboxColor.SelectedItem.ToString());
+        }
+        private void btnRandomColor_Click(object sender, EventArgs e)
+        {
+            RenkDegistir();
+        }
+
+        private void btnWC_Click(object sender, EventArgs e)
+        {
+            Color c = ArkaPlanRengi();
+            MessageBox.Show(c.Name);
+            //System.Diagnostics.Process.Start("http://www.YavuzGedik.com"); 
         }
     }
 }
