c1ac4e92e0bb733c8f9a1772bb4c6cbe03212762
yavuzgedik <yavuzgedik@windowslive.com>
2017-08-17 21:31:32 +0300

    OutRefParams Eklendi

---
+++ OutRefParams/OutRefParams/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace OutRefParams
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOut1 = new System.Windows.Forms.Button();
+            this.btnOut2 = new System.Windows.Forms.Button();
+            this.btnParams = new System.Windows.Forms.Button();
+            this.btnRef = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnOut1
+            // 
+            this.btnOut1.Location = new System.Drawing.Point(12, 12);
+            this.btnOut1.Name = "btnOut1";
+            this.btnOut1.Size = new System.Drawing.Size(260, 39);
+            this.btnOut1.TabIndex = 0;
+            this.btnOut1.Text = "OUT 1";
+            this.btnOut1.UseVisualStyleBackColor = true;
+            this.btnOut1.Click += new System.EventHandler(this.btnOut1_Click);
+            // 
+            // btnOut2
+            // 
+            this.btnOut2.Location = new System.Drawing.Point(12, 57);
+            this.btnOut2.Name = "btnOut2";
+            this.btnOut2.Size = new System.Drawing.Size(260, 39);
+            this.btnOut2.TabIndex = 1;
+            this.btnOut2.Text = "OUT 2";
+            this.btnOut2.UseVisualStyleBackColor = true;
+            this.btnOut2.Click += new System.EventHandler(this.btnOut2_Click);
+            // 
+            // btnParams
+            // 
+            this.btnParams.Location = new System.Drawing.Point(12, 102);
+            this.btnParams.Name = "btnParams";
+            this.btnParams.Size = new System.Drawing.Size(260, 39);
+            this.btnParams.TabIndex = 2;
+            this.btnParams.Text = "PARAMS";
+            this.btnParams.UseVisualStyleBackColor = true;
+            this.btnParams.Click += new System.EventHandler(this.btnParams_Click);
+            // 
+            // btnRef
+            // 
+            this.btnRef.Location = new System.Drawing.Point(12, 147);
+            this.btnRef.Name = "btnRef";
+            this.btnRef.Size = new System.Drawing.Size(260, 39);
+            this.btnRef.TabIndex = 3;
+            this.btnRef.Text = "REF";
+            this.btnRef.UseVisualStyleBackColor = true;
+            this.btnRef.Click += new System.EventHandler(this.btnRef_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 199);
+            this.Controls.Add(this.btnRef);
+            this.Controls.Add(this.btnParams);
+            this.Controls.Add(this.btnOut2);
+            this.Controls.Add(this.btnOut1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOut1;
+        private System.Windows.Forms.Button btnOut2;
+        private System.Windows.Forms.Button btnParams;
+        private System.Windows.Forms.Button btnRef;
+    }
+}
+
---
+++ OutRefParams/OutRefParams/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OutRefParams
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        void GeriyeDegerDon(int sayi, out double firlatilanSonuc)
+        {
+            firlatilanSonuc = Math.Pow(sayi, 2);
+        }
+
+        private void btnOut1_Click(object sender, EventArgs e)
+        {
+            double yakalananSonuc;
+            GeriyeDegerDon(12, out yakalananSonuc);
+            MessageBox.Show(yakalananSonuc.ToString());
+        }
+
+        void Hesapla(int sayi1, int sayi2, out int toplam, out int cikarma, out long carpma, out double bolme, out byte mod)
+        {
+            toplam = sayi1 + sayi2;
+            cikarma = sayi1 - sayi2;
+            carpma = sayi1 * sayi2;
+            bolme = sayi1 / sayi2;
+            mod = (byte)(sayi1 % sayi2);
+        }
+        private void btnOut2_Click(object sender, EventArgs e)
+        {
+            int toplam, cikarma;
+            long carpma;
+            double bolme;
+            byte mod;
+
+            Hesapla(8, 3, out toplam, out cikarma, out carpma, out bolme, out mod);
+        }
+
+        double FiyatHesapla(params double[] fiyatlar)
+        {
+            double toplamFiyat = 0;
+            foreach (var item in fiyatlar)
+            {
+                toplamFiyat += item;
+            }
+            return toplamFiyat;
+        }
+        private void btnParams_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(FiyatHesapla(193.20,56.55,68.74,123.54,02.03).ToString());
+        }
+
+        void Arttir(ref int sayi)
+        {
+            sayi++;
+        }
+        private void btnRef_Click(object sender, EventArgs e)
+        {
+            int number = 8;
+            Arttir(ref number);
+            MessageBox.Show(number.ToString());
+        }
+    }
+}
