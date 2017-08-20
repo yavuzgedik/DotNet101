96d48162051e36d13b0eed4f081b4abaec09c276
yavuzgedik <yavuzgedik@outlook.com>
2017-08-20 23:59:08 +0300

    Comment Lines Added

--- 04Degiskenler/04Degiskenler/Form1.cs
+++ 04Degiskenler/04Degiskenler/Form1.cs
@@ -13,20 +13,29 @@ namespace _04Degiskenler
     public partial class Form1 : Form
     {
         // Değişkenler, verileri kaydettiğimiz alanlardır.
-        
-        byte sayi; // 0-255 arasında bir değer ataması yapılabilir
-        string metin; // metinsel ifadeler tutulur
+
+        // Herhangi bir metot içine yazılmayan değişkenler, GLOBAL değişkenler olarak adlandırılır.
+        // Class dışında değişken tanımlaması yapılamaz.
+        byte sayi; // 0-255 arasında bir değer ataması yapılabilir.
+        string metin; // metinsel ifadeler tutulur.
 
         public Form1()
         {
+            // InitialComponent metotu form ve form üzerinde nesnelerin oluşturan (görsel olarak) fonksiyondur.
             InitializeComponent();
+
+            // sayi ve metin değişkenleri global değişken olduğu için, bu metot içinde ilgili değişkenlere erişip kullanabiliyorum.
             sayi = 5;
             metin = "2017 yılındayız.";
 
+            /*
+             * değişen tiplerinin üzerine tıklayıp F12'ye basarak ilgili tipin ayrıntılarını görebilirsiniz.
+             * Veya değişen tiplerinin üzerine sağ tık yapıp Go To Definition seçeneği ile aynı işlevi yapabilirsiniz.
+             */
             byte number;
-            sbyte numder2; // -128 127 arasınde bir değer alır
+            sbyte numder2; // -128 127 arasınde bir değer alır.
 
-            short number3; // short secili iken F12'ye basarak ayrıntılara gidilebilir
+            short number3; // short secili iken F12'ye basarak ayrıntılara gidilebilir.
             ushort number4;
 
             int number5;
@@ -42,8 +51,9 @@ namespace _04Degiskenler
 
             string isim = "Yavuz Gedik";
             string harf = "a";
-            char c = 'y';
+            char c = 'y'; // karakter tipi
 
+            // True (1) veya False (0) değerlerini tutulur.
             bool bugunTatilMi = false;
             bool bugunDersVarMi = true;
             bool result = 10 > 2;
@@ -56,15 +66,19 @@ namespace _04Degiskenler
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show(sayi.ToString());
+            // MessageBox.Show metotu ile ekranda mesaj gösteriyoruz.
+            // Bu metot içine string bir ifade yazmamız gerekiyor.
+            MessageBox.Show(sayi.ToString()); // sayi değişkenin tipi Byte olduğu ve ekranda değerini yazmamız için ToString() metotu ile string tipine dönüştürülür.
+            
+            // sayi değişkeni global değişken olduğu için, bu metot içinde erişip kullanabiliyorum.
             sayi = 10;
             MessageBox.Show(sayi.ToString());
-            MessageBox.Show(metin);
+            MessageBox.Show(metin); // metin değişkeninin tipi string olduğu için ToString() metotu kullanımına gerek yoktur.
         }
 
         private void btnMerhaba_Click(object sender, EventArgs e)
         {
-            string gelenMetin = txtMetin.Text;
+            string gelenMetin = txtMetin.Text; // txtMetin adlı TextBox'ın Text değeri ilgili değişkene atanıyor. (Text özelliği string'dir.)
             MessageBox.Show("Merhaba " + gelenMetin);
             btnMerhaba.Text = gelenMetin;
         }
@@ -76,7 +90,7 @@ namespace _04Degiskenler
 
         private void BtnZaman_Click(object sender, EventArgs e)
         {
-            DateTime zaman = new DateTime();
+            DateTime zaman = new DateTime(); // Tarih, Zaman ve Saat işlemleri için kullandığımız Class ve Tip
             zaman = DateTime.Now;
 
             MessageBox.Show(zaman.ToString());
