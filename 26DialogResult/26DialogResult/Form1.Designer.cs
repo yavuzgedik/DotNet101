1b881f58795192b0624436a5530df3cbc7726895
yavuzgedik <yavuzgedik@windowslive.com>
2017-08-24 21:21:56 +0300

    DialogResult, StopWatch, Timer

---
+++ 26DialogResult/26DialogResult/Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace _26DialogResult
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listAgrubu = new System.Windows.Forms.ListBox();
+            this.listBgrubu = new System.Windows.Forms.ListBox();
+            this.listCgrubu = new System.Windows.Forms.ListBox();
+            this.listDgrubu = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.listTakimlar = new System.Windows.Forms.ListBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnKura = new System.Windows.Forms.Button();
+            this.btnKura2 = new System.Windows.Forms.Button();
+            this.btnKura3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listAgrubu
+            // 
+            this.listAgrubu.FormattingEnabled = true;
+            this.listAgrubu.ItemHeight = 20;
+            this.listAgrubu.Location = new System.Drawing.Point(161, 37);
+            this.listAgrubu.Name = "listAgrubu";
+            this.listAgrubu.Size = new System.Drawing.Size(103, 104);
+            this.listAgrubu.TabIndex = 0;
+            // 
+            // listBgrubu
+            // 
+            this.listBgrubu.FormattingEnabled = true;
+            this.listBgrubu.ItemHeight = 20;
+            this.listBgrubu.Location = new System.Drawing.Point(270, 37);
+            this.listBgrubu.Name = "listBgrubu";
+            this.listBgrubu.Size = new System.Drawing.Size(103, 104);
+            this.listBgrubu.TabIndex = 1;
+            // 
+            // listCgrubu
+            // 
+            this.listCgrubu.FormattingEnabled = true;
+            this.listCgrubu.ItemHeight = 20;
+            this.listCgrubu.Location = new System.Drawing.Point(379, 37);
+            this.listCgrubu.Name = "listCgrubu";
+            this.listCgrubu.Size = new System.Drawing.Size(103, 104);
+            this.listCgrubu.TabIndex = 2;
+            // 
+            // listDgrubu
+            // 
+            this.listDgrubu.FormattingEnabled = true;
+            this.listDgrubu.ItemHeight = 20;
+            this.listDgrubu.Location = new System.Drawing.Point(488, 37);
+            this.listDgrubu.Name = "listDgrubu";
+            this.listDgrubu.Size = new System.Drawing.Size(103, 104);
+            this.listDgrubu.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(157, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "A Grubu";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(270, 14);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "B Grubu";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(379, 14);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "C Grubu";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(484, 14);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(70, 20);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "D Grubu";
+            // 
+            // listTakimlar
+            // 
+            this.listTakimlar.FormattingEnabled = true;
+            this.listTakimlar.ItemHeight = 20;
+            this.listTakimlar.Location = new System.Drawing.Point(12, 37);
+            this.listTakimlar.Name = "listTakimlar";
+            this.listTakimlar.Size = new System.Drawing.Size(120, 324);
+            this.listTakimlar.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 14);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(68, 20);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Takımlar";
+            // 
+            // btnKura
+            // 
+            this.btnKura.Location = new System.Drawing.Point(12, 367);
+            this.btnKura.Name = "btnKura";
+            this.btnKura.Size = new System.Drawing.Size(120, 56);
+            this.btnKura.TabIndex = 10;
+            this.btnKura.Text = "KURA";
+            this.btnKura.UseVisualStyleBackColor = true;
+            this.btnKura.Click += new System.EventHandler(this.btnKura_Click);
+            // 
+            // btnKura2
+            // 
+            this.btnKura2.Location = new System.Drawing.Point(144, 367);
+            this.btnKura2.Name = "btnKura2";
+            this.btnKura2.Size = new System.Drawing.Size(120, 56);
+            this.btnKura2.TabIndex = 11;
+            this.btnKura2.Text = "KURA 2";
+            this.btnKura2.UseVisualStyleBackColor = true;
+            this.btnKura2.Click += new System.EventHandler(this.btnKura2_Click);
+            // 
+            // btnKura3
+            // 
+            this.btnKura3.Location = new System.Drawing.Point(274, 367);
+            this.btnKura3.Name = "btnKura3";
+            this.btnKura3.Size = new System.Drawing.Size(120, 56);
+            this.btnKura3.TabIndex = 12;
+            this.btnKura3.Text = "KURA 1 (for)";
+            this.btnKura3.UseVisualStyleBackColor = true;
+            this.btnKura3.Click += new System.EventHandler(this.btnKura3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(603, 435);
+            this.Controls.Add(this.btnKura3);
+            this.Controls.Add(this.btnKura2);
+            this.Controls.Add(this.btnKura);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.listTakimlar);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listDgrubu);
+            this.Controls.Add(this.listCgrubu);
+            this.Controls.Add(this.listBgrubu);
+            this.Controls.Add(this.listAgrubu);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listAgrubu;
+        private System.Windows.Forms.ListBox listBgrubu;
+        private System.Windows.Forms.ListBox listCgrubu;
+        private System.Windows.Forms.ListBox listDgrubu;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListBox listTakimlar;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnKura;
+        private System.Windows.Forms.Button btnKura2;
+        private System.Windows.Forms.Button btnKura3;
+    }
+}
+
---
+++ 26DialogResult/26DialogResult/Form1.cs
@@ -0,0 +1,257 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _26DialogResult
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Timer timer = new Timer();
+        Random rnd = new Random();
+        List<string> takimlar = new List<string>();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer.Interval = 1;
+            timer.Tick += Timer_Tick;
+            TakimOlustur();
+
+            foreach (var item in takimlar)
+            {
+                listTakimlar.Items.Add(item);
+            }
+        }
+
+        int count = 0;
+        int tickCount = 0;
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            tickCount++;
+            int indexGrup = rnd.Next(1, 5);
+
+            switch (indexGrup)
+            {
+                case 1:
+                    if (listAgrubu.Items.Count < 4)
+                    {
+                        listAgrubu.Items.Add(takimlar[count]);
+                    }
+                    else
+                    {
+                        count--;
+                        break;
+                    }
+                    break;
+                case 2:
+                    if (listBgrubu.Items.Count < 4)
+                    {
+                        listBgrubu.Items.Add(takimlar[count]);
+                    }
+                    else
+                    {
+                        count--;
+                        break;
+                    }
+                    break;
+                case 3:
+                    if (listCgrubu.Items.Count < 4)
+                    {
+                        listCgrubu.Items.Add(takimlar[count]);
+                    }
+                    else
+                    {
+                        count--;
+                        break;
+                    }
+                    break;
+                case 4:
+                    if (listDgrubu.Items.Count < 4)
+                    {
+                        listDgrubu.Items.Add(takimlar[count]);
+                    }
+                    else
+                    {
+                        count--;
+                        break;
+                    }
+                    break;
+                default:
+                    break;
+            }
+
+            count++;
+
+            if (count > 15)
+            {
+                timer.Stop();
+                sw.Stop();
+                Text = tickCount.ToString();
+                Text = sw.ElapsedMilliseconds.ToString();
+            }
+        }
+
+        Stopwatch sw = new Stopwatch();
+        private void btnKura_Click(object sender, EventArgs e)
+        {
+            DialogResult result =
+                MessageBox.Show
+                (
+                    "Emin Misin?",
+                    "Onay Ekranı",
+                    MessageBoxButtons.OKCancel
+                );
+
+            if (result == DialogResult.OK)
+            {
+                Temizle();
+                timer.Start();
+                sw.Restart();
+            }
+            else
+            {
+                MessageBox.Show("Onay Verilmedi");
+            }
+        }
+
+        private void btnKura2_Click(object sender, EventArgs e)
+        {
+            sw.Restart();
+            Temizle();
+
+            List<string> geciciList = new List<string>();
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    int index = rnd.Next(0, takimlar.Count);
+                    geciciList.Add(takimlar[index]);
+                    takimlar.RemoveAt(index);
+                }
+
+                string takim = geciciList[rnd.Next(0, geciciList.Count)];
+                listAgrubu.Items.Add(takim);
+                geciciList.Remove(takim);
+
+                takim = geciciList[rnd.Next(0, geciciList.Count)];
+                listBgrubu.Items.Add(takim);
+                geciciList.Remove(takim);
+
+                takim = geciciList[rnd.Next(0, geciciList.Count)];
+                listCgrubu.Items.Add(takim);
+                geciciList.Remove(takim);
+
+                takim = geciciList[rnd.Next(0, geciciList.Count)];
+                listDgrubu.Items.Add(takim);
+                geciciList.Remove(takim);
+            }
+            sw.Stop();
+            Text = sw.ElapsedMilliseconds.ToString();
+            TakimOlustur();
+        }
+
+        void Temizle()
+        {
+            listAgrubu.Items.Clear();
+            listBgrubu.Items.Clear();
+            listCgrubu.Items.Clear();
+            listDgrubu.Items.Clear();
+            count = 0;
+            tickCount = 0;
+        }
+
+        void TakimOlustur()
+        {
+            takimlar.Add("Barcelona");
+            takimlar.Add("Real Madrid");
+            takimlar.Add("PSG");
+            takimlar.Add("Monaco");
+            takimlar.Add("Lyon");
+            takimlar.Add("Bayern Munih");
+            takimlar.Add("Liverpool");
+            takimlar.Add("Galatasaray");
+            takimlar.Add("Juventus");
+            takimlar.Add("Milan");
+            takimlar.Add("Inter");
+            takimlar.Add("Porto");
+            takimlar.Add("Beşiktaş");
+            takimlar.Add("Chelsea");
+            takimlar.Add("B. Dortmund");
+            takimlar.Add("Napoli");
+        }
+
+        private void btnKura3_Click(object sender, EventArgs e)
+        {
+            Temizle();
+
+            sw.Restart();
+            for (int i = 0; i < 16; i++)
+            {
+                int indexGrup = rnd.Next(1, 5);
+
+                switch (indexGrup)
+                {
+                    case 1:
+                        if (listAgrubu.Items.Count < 4)
+                        {
+                            listAgrubu.Items.Add(takimlar[i]);
+                        }
+                        else
+                        {
+                            i--;
+                            break;
+                        }
+                        break;
+                    case 2:
+                        if (listBgrubu.Items.Count < 4)
+                        {
+                            listBgrubu.Items.Add(takimlar[i]);
+                        }
+                        else
+                        {
+                            i--;
+                            break;
+                        }
+                        break;
+                    case 3:
+                        if (listCgrubu.Items.Count < 4)
+                        {
+                            listCgrubu.Items.Add(takimlar[i]);
+                        }
+                        else
+                        {
+                            i--;
+                            break;
+                        }
+                        break;
+                    case 4:
+                        if (listDgrubu.Items.Count < 4)
+                        {
+                            listDgrubu.Items.Add(takimlar[i]);
+                        }
+                        else
+                        {
+                            i--;
+                            break;
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            sw.Stop();
+            Text = sw.ElapsedMilliseconds.ToString();
+        }
+    }
+}
