e21de1f3c9f988a42abaed3a00ea349276133826
yavuzgedik <yavuzgedik@outlook.com>
2017-09-21 21:25:56 +0300

    Added First SQL File

---
+++ SQLQuery/SQLQuery1.sql
@@ -0,0 +1,17 @@
+INSERT INTO Filmler (YapimYili, FilmAdi, FilmNo) 
+VALUES('2016-6-6','DAÐ 2',2)
+
+INSERT INTO Filmler VALUES(1,'DAÐ','2015-6-6')
+
+SELECT * FROM Filmler
+
+--CREATE TABLE Filmler
+--(
+--	FilmNo int not null,
+--	FilmAdi nvarchar(50) not null,
+--	YapimYili Datetime not null
+--)
+
+--char(n) char(10) "yavuz" 10
+--varchar(n) varchar(10) "yavuz" 5
+--nvarchar(n) nvarchar(10) "yavuz" 5 "þ"
