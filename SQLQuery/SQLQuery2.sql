e92387d7b43aa9a04b8a6e8039036d88725e3eab
Yavuz Gedik <yavuzgedik@outlook.com>
2017-09-25 21:16:35 +0300

    Create SQLQuery2.sql

---
+++ SQLQuery/SQLQuery2.sql
@@ -0,0 +1,23 @@
+SELECT FilmAdi, YapimYili FROM Filmler
+
+SELECT OyuncuAd FROM Oyuncular
+
+SELECT * FROM Oyuncular
+
+INSERT INTO Oyuncular VALUES ('Yavuz Gedik')
+
+--CREATE TABLE Oyuncular
+--(
+--	OyuncuNo int not null PRIMARY KEY IDENTITY(1,1),
+--	OyuncuAd nvarchar(50)
+--)
+
+--CREATE TABLE Yonetmenler
+--(
+--	YonetmenNo int not null PRIMARY KEY,
+--	YonetmenAd nvarchar(50)
+--)
+
+SELECT * FROM Filmler
+
+INSERT INTO Filmler VALUES ('Geleceğe Dönüş 1', '1985-06-06');
